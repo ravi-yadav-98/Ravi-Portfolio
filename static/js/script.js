d26c9a0a5f6d8ab6d72b8d1893a4ac84645ec7d1
Ravi Prakash Yadav <raviprakashyadav1998@gmail.com>
2023-07-28 15:57:36 +0530

    updated

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+// function submitForm() {
+    
+//     const scriptURL =""
+//     const form = document.forms['qform']
+//     form.addEventListener('submit', e =>
+//     {
+//         e.preventDefault()
+//         fetch(scriptURL, {method: 'POST', body: new formData(form)})
+//         .then(response => alert("Thansk for submitting the form"))
+//         .catch(error => console.error('Errror!', error.message))
+//     })
+// }
